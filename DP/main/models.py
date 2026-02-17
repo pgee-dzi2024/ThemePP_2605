50d0bd2f7edd82a9314f34b02efd340ae602241c
Алекс Кулин <kulinaleksander@gmail.com>
2026-02-17 13:06:49 +0200

    Модел

--- DP/main/models.py
+++ DP/main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Comment(models.Model):
+    subject = models.CharField(max_length=200)
+    content = models.TextField()
+    user = models.CharField(max_length=100)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.subject
+
+    class Meta:
+        db_table = "comment"
+        verbose_name = "comment"
+        verbose_name_plural = "comments"
+        ordering = ['-date']
