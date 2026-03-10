7958f355ad259d7de0bc0538c9656d81d338d209
Алекс Кулин <kulinaleksander@gmail.com>
2026-03-10 11:03:19 +0200

    Преди тест

--- DP/main/models.py
+++ DP/main/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Comment(models.Model):
-    subject = models.CharField(max_length=200)
+    subject = models.CharField(max_length=200, default="No Subject")
     content = models.TextField()
-    user = models.CharField(max_length=100)
+    user = models.CharField(max_length=100, default="Anonymous")
     date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
